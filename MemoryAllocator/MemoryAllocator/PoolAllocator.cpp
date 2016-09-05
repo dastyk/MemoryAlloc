1680cc9710f30188595b6203218c37a613eb5638
dastyk <dastykill@gmail.com>
2016-09-05 11:57:40 +0200

    Malloc and Free

--- MemoryAllocator/MemoryAllocator/PoolAllocator.cpp
+++ MemoryAllocator/MemoryAllocator/PoolAllocator.cpp
@@ -1,6 +1,6 @@
 #include "PoolAllocator.h"
 #include <memory>
-
+#include <stdexcept>
 
 PoolAllocator::PoolAllocator(size_t blocksize, size_t numBlocks) : _blockSize(blocksize), _numBlocks(numBlocks)
 {
@@ -17,25 +17,34 @@ PoolAllocator::~PoolAllocator()
 
 inline void * PoolAllocator::Malloc()
 {
-	return nullptr;
+	if (!_free) throw std::runtime_error("No more memory.");
+
+	void* ret = _free; // Make copy for return.
+	memcpy(&_free, _free, sizeof(void*)); // Copy the value that _free points to, to the variable _free.
+	return ret;
 }
 
-inline void PoolAllocator::Free(void *)
+inline void PoolAllocator::Free(void * p)
 {
+	if (!p) throw std::runtime_error("Free on nullptr");
+	void* prev = _free; // Make copy of previous free block.
+	memcpy(&_free, p, sizeof(void*)); // Free the given block and set it as next free block.
+	memcpy(_free, &prev, sizeof(void*)); // Set the previous next free block as the next next free block.
 }
 
 void PoolAllocator::_SetupFreeBlocks()
 {
 	char* p = reinterpret_cast<char*>(_pool);
-	char* nextPointer = p + _blockSize;
 
 	// Iterate through blocks (all are free at first) and set the first bytes
 	// to the pointer of the next block, thereby creating a linked list.
-	for (uint32_t i = 0; i < _numBlocks; ++i)
+	for (uint32_t i = 0; i < _numBlocks-1; ++i)
 	{
+		char* nextPointer = p + _blockSize;
 		memcpy(p, &nextPointer, sizeof(nextPointer));
 		p += _blockSize;
 	}
-
+	char* nextPointer = nullptr;
+	memcpy(p, &nextPointer, sizeof(nextPointer));
 	_free = _pool;
 }
