482ac819ab75d96f22443a504a8b082bda00650c
dastyk <dastykill@gmail.com>
2016-09-05 11:37:06 +0200

    Setup initial linked list of free blocks.

--- MemoryAllocator/MemoryAllocator/PoolAllocator.cpp
+++ MemoryAllocator/MemoryAllocator/PoolAllocator.cpp
@@ -1,5 +1,5 @@
 #include "PoolAllocator.h"
-
+#include <memory>
 
 
 PoolAllocator::PoolAllocator(size_t blocksize, size_t numBlocks) : _blockSize(blocksize), _numBlocks(numBlocks)
@@ -26,5 +26,16 @@ inline void PoolAllocator::Free(void *)
 
 void PoolAllocator::_SetupFreeBlocks()
 {
-	
+	char* p = reinterpret_cast<char*>(_pool);
+	char* nextPointer = p + _blockSize;
+
+	// Iterate through blocks (all are free at first) and set the first bytes
+	// to the pointer of the next block, thereby creating a linked list.
+	for (uint32_t i = 0; i < _numBlocks; ++i)
+	{
+		memcpy(p, &nextPointer, sizeof(nextPointer));
+		p += _blockSize;
+	}
+
+	_free = _pool;
 }
