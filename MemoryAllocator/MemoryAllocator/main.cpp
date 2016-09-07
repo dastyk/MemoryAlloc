403f59f35029bd54a353e587fe0547ed7b482fa3
dastyk <dastykill@gmail.com>
2016-09-07 15:11:45 +0200

    Went back to pointers for pool allocator linked list

--- MemoryAllocator/MemoryAllocator/main.cpp
+++ MemoryAllocator/MemoryAllocator/main.cpp
@@ -11,12 +11,26 @@
 #include <chrono>       // std::chrono::system_clock
 int main()
 {
-	TestCaseC test;
+	/*TestCaseC test;
 
 	test.TestPoolAllocatorThreaded<Enemies>();
 	test.TestStackAllocatorThreaded<Enemies>();
 	test.TestPoolAllocator<Enemies>();
-	test.TestStackAllocator<Enemies>();
+	test.TestStackAllocator<Enemies>();*/
+
+
+	MemoryManager m(2U * 1024U * 1024U * 1024U);
+	PoolAllocator* pool = m.CreatePoolAllocator(sizeof(uint64_t), 1000000);
+
+   uint64_t* i = (uint64_t*)pool->Malloc();
+	*i = 5;
+
+   uint64_t& a = *(uint64_t*)pool->Malloc();
+
+	a = 6;
+
+	std::cout << "i: " << *i << std::endl << "a: " << a << std::endl;
+	
 
 	system("pause");
 	return 0;
