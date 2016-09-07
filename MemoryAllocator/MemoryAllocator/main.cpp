9606937a889bd0d2f92b6b77118637333cb02036
dastyk <andenzz@hotmail.com>
2016-09-07 16:17:31 +0200

    Catching exception for pool allocator error

--- MemoryAllocator/MemoryAllocator/main.cpp
+++ MemoryAllocator/MemoryAllocator/main.cpp
@@ -4,10 +4,17 @@ int main()
 {
 	TestCaseC test;
 
-	test.TestPoolAllocatorThreaded<Enemies>();
-	test.TestStackAllocatorThreaded<Enemies>();
-	test.TestPoolAllocator<Enemies>();
-	test.TestStackAllocator<Enemies>();
+   try
+   {
+      test.TestPoolAllocatorThreaded<Enemies>();
+      test.TestStackAllocatorThreaded<Enemies>();
+      test.TestPoolAllocator<Enemies>();
+      test.TestStackAllocator<Enemies>();
+   }
+   catch (std::runtime_error& err)
+   {
+      std::cout << err.what() << std::endl;
+   }
 
 	system("pause");
 	return 0;
