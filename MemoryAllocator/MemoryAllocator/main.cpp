c5d0ae4bdad4cde1e12236fc42a57c184acd8abb
dastyk <andenzz@hotmail.com>
2016-09-07 16:07:16 +0200

    Cleaned up pool allocator a bit

--- MemoryAllocator/MemoryAllocator/main.cpp
+++ MemoryAllocator/MemoryAllocator/main.cpp
@@ -1,14 +1,5 @@
 #include "TestCaseController.h"
 
-#include "Timer.h"
-#include "PoolAllocator.h"
-#include "MemoryManager.h"
-#include <iostream>
-#include "TestStructs.h"
-#include <algorithm>    // std::shuffle
-#include <array>        // std::array
-#include <random>       // std::default_random_engine
-#include <chrono>       // std::chrono::system_clock
 int main()
 {
 	TestCaseC test;
