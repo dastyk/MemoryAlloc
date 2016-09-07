32e18609740971f765aab4b4ffe54d6504e7d209
Per <per_strom@live.se>
2016-09-07 11:54:25 +0200

    Added aligned allocation function to the stack allocator.

--- MemoryAllocator/MemoryAllocator/StackAllocator.h
+++ MemoryAllocator/MemoryAllocator/StackAllocator.h
@@ -13,11 +13,35 @@ public:
 		if (_marker + size > _stackSize)
 			throw std::runtime_error("Stack allocator out of memory.");
 
-		size_t memloc = _marker;
+		size_t stackPos = _marker;
 		_marker += size;
 
-		return reinterpret_cast<char*>(_stack) + memloc;
+		return reinterpret_cast<char*>(_stack) + stackPos;
 
+
+	}
+	
+	inline void* AllocAligned(size_t size, uint32_t alignment)
+	{
+		//Here's the code that is actually readable
+		/*
+		uint32_t mask = alignment - 1;
+		size_t address = reinterpret_cast<uintptr_t>(_stack) + _marker;
+		uint32_t misalignment = mask & address;
+		uint32_t adjustment = alignment - misalignment;
+		if (_marker + size + adjustment > _stackSize)
+			throw std::runtime_error("Stack allocator out of memory.");
+		size_t stackPos = _marker + adjustment;
+		_marker += size + adjustment;
+		return reinterpret_cast<char*>(_stack) + stackPos;
+		*/
+		//Here's the same code as above but declaring only 1 variable because possibly marginally faster
+		size_t address = (reinterpret_cast<uintptr_t>(_stack) + _marker) & (alignment - 1);
+		address = alignment - address + _marker;
+		if(address + size > _stackSize)
+			throw std::runtime_error("Stack allocator out of memory.");
+		_marker = address;
+		return reinterpret_cast<char*>(_stack) + address;
 
 	}
 
