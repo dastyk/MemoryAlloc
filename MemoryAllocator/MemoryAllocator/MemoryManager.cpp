e8357815bd0db314d13fca20f58b97170229f69b
dastyk <dastykill@gmail.com>
2016-09-09 10:39:23 +0200

    Can create unaligned

--- MemoryAllocator/MemoryAllocator/MemoryManager.cpp
+++ MemoryAllocator/MemoryAllocator/MemoryManager.cpp
@@ -12,26 +12,36 @@ MemoryManager::~MemoryManager()
 	delete[] _memory;
 }
 
-PoolAllocator * MemoryManager::CreatePoolAllocator(uint32_t sizeOfObject, uint32_t nrOfObjects)
+PoolAllocator * MemoryManager::CreatePoolAllocator(uint32_t sizeOfObject, uint32_t nrOfObjects, uint8_t flag = POOL_ALLOCATOR_ALIGNED)
 {
-	_mutexLock.lock();
-	uint64_t alignment = (uint64_t)__max(8, powl(2, ceil(log2l((long double)sizeOfObject))));
-	
-	char* rawAddress = reinterpret_cast<char*>(_free) + sizeof(PoolAllocator);
-	uint64_t mask = alignment - 1;
-	uint64_t misalignment = mask & (size_t)rawAddress;
-	uint64_t adjustment = alignment - misalignment;
-	uint64_t requirement = alignment*nrOfObjects + adjustment + sizeof(PoolAllocator);
-	if (_remainingMemory < (requirement))
+	if (flag & POOL_ALLOCATOR_ALIGNED)
 	{
-		throw std::exception("Not enough memory");
+		_mutexLock.lock();
+		uint64_t alignment = (uint64_t)__max(8, powl(2, ceil(log2l((long double)sizeOfObject))));
+
+		char* rawAddress = _free + sizeof(PoolAllocator);
+		uint64_t mask = alignment - 1;
+		uint64_t misalignment = mask & (size_t)rawAddress;
+		uint64_t adjustment = alignment - misalignment;
+		uint64_t requirement = alignment*nrOfObjects + adjustment + sizeof(PoolAllocator);
+		if (_remainingMemory < (requirement))
+		{
+			throw std::exception("Not enough memory");
+		}
+		_remainingMemory -= requirement;
+		char* alignedAddress = rawAddress + adjustment;
+		PoolAllocator* pool = new(_free) PoolAllocator(alignedAddress, alignment, nrOfObjects);
+		_free += requirement;
+		_mutexLock.unlock();
+		return pool;
 	}
-	_remainingMemory -= requirement;
-	char* alignedAddress = rawAddress + adjustment;
-	PoolAllocator* pool = new(_free) PoolAllocator((char*)alignedAddress, alignment, nrOfObjects);
-	_free += requirement;
-	_mutexLock.unlock();
-	return pool;
+	else
+	{
+		char* rawAdd = _free + sizeof(PoolAllocator);
+		PoolAllocator* pool = new(_free) PoolAllocator(rawAdd, sizeOfObject, nrOfObjects);
+		_free = rawAdd + sizeOfObject*nrOfObjects;
+		return pool;
+	}
 }
 
 StackAllocator * MemoryManager::CreateStackAllocator(uint64_t size)
